927356026a4bc0890c73e1f3ebdfb6ce4e9b3e85
LSH <dltpgustpgus@gmail.com>
2023-12-07 16:42:59 +0900

    instaweb-v2 프로젝트 시작, bootstrap, ckeditor 라이브러리 추가

---
+++ src/main/java/com/lsh/instawebv2/InstawebV2Application.java
@@ -0,0 +1,13 @@
+package com.lsh.instawebv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InstawebV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InstawebV2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/lsh/instawebv2/InstawebV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lsh.instawebv2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InstawebV2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
