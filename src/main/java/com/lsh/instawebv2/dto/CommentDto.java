6108e508a646895cfd00bb9b8bbeebdc3a6300d0
LSH <dltpgustpgus@gmail.com>
2023-12-27 20:56:34 +0900

    feat(comment): 댓글에 작성자 username 이 아닌 email 보이도록 수정

--- src/main/java/com/lsh/instawebv2/dto/CommentDto.java
+++ src/main/java/com/lsh/instawebv2/dto/CommentDto.java
@@ -11,11 +11,13 @@ public class CommentDto {
     private String commentContent;
     private LocalDateTime createdTime;
     private String memberName;
+    private String email;
 
     public CommentDto(Comment comment) {
         this.id = comment.getId();
         this.commentContent = comment.getCommentContent();
         this.createdTime = comment.getCreatedTime();
         this.memberName = comment.getMember().getUsername();
+        this.email = comment.getMember().getEmail();
     }
 }
